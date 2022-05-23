6df42b74d373e63a92a7eb25cda0365200c632f7
187PM14032 - Nguyen Anh Tuan - K24T-PM2 <tuan.187pm14032@vanlanguni.vn>
2022-05-24 06:28:41 +0700

    add Router/layout

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+//Layouts
+import { HeaderOnly } from '~/components/Layout';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+
+//Public routes
+export const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+
+export const privateRoutes = [];
